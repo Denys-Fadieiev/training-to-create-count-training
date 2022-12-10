bc933df8e7932bcd4175e19ea401afb3d5d9d2b5
Denys-Fadieiev <dencheek0911@gmail.com>
2022-12-10 21:30:08 +0200

    add constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+const CONSTANTS = {
+  LANGUAGE:{
+    EN_US: { VALUE: 'en_us', OPTION_TEXT: 'English' },
+    UA_UA: { VALUE: 'ua_ua', OPTION_TEXT: 'Українська' },
+    RU_RU: { VALUE: 'ru_ru', OPTION_TEXT: 'русский'},
+  },
+}
+
+export default CONSTANTS;
