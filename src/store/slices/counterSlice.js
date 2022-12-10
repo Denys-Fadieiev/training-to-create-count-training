fdd908a6b2edb7dab6002e9b2b2f9cd8c086b9b9
Denys-Fadieiev <dencheek0911@gmail.com>
2022-12-10 20:34:45 +0200

    add counterSlice

---
+++ src/store/slices/counterSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+  step: 1,
+}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state, action)=>{
+      state.count += state.step;
+    },
+    decrement: (state, action) => {
+      state.count -= state.step;
+    },
+    setStep: (state, action) => {
+      state.step = Number(action.payload);
+    },
+  },
+});
+
+const { reducer, actions } = counterSlice;
+const { increment, decrement, setStep } = actions;
+
+export { increment, decrement, setStep };
+export default reducer;
